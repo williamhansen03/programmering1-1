ecf59775c5a0ee5fc81de83b20c00a0d6dd0fcd3
fluff-otto <abbotto23@gmail.com>
2022-01-21 10:06:19 +0100

    Update avengersleague.py

--- avengersleague.py
+++ avengersleague.py
@@ -1,5 +1,5 @@
-printprint("Hello World")
+import random
 
 avenger_list = ["Spiderman", "Iron Man", "Thor", "Vision", "Captain America", "Hulk"]
 
-dc_list = ["Batman", "Joker", "Aquaman", "Flash", "Bane", "Green Lantern"]
+dc_list = ["Batman", "Joker", "Aquaman", "Flash", "Superman", "Green Lantern"]
