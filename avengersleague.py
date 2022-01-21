3850492cd0cd064746af2b6558e823abfac76218
fluff-otto <abbotto23@gmail.com>
2022-01-21 09:59:34 +0100

    Update avengersleague.py
    bruh

--- avengersleague.py
+++ avengersleague.py
@@ -1,1 +1,5 @@
 print("Hello World")
+
+avenger_list = ["Spiderman", "Iron Man", "Thor", "Vision", "Captain America", "Hulk"]
+
+dc_list = ["Batman", "Joker", "Aquaman", "Flash", "Bane", "Green Lantern"]
