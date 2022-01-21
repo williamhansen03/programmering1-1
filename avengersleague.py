b8630919022338d21aebf73625cc5d44a287cd6d
fluff-otto <abbotto23@gmail.com>
2022-01-21 10:51:17 +0100

    Update avengersleague.py

--- avengersleague.py
+++ avengersleague.py
@@ -1,5 +1,18 @@
 import random
 
-avenger_list = ["Spiderman", "Iron Man", "Thor", "Vision", "Captain America", "Hulk"]
+lastnumber = 0
+firstnumber = 0
 
-dc_list = ["Batman", "Joker", "Aquaman", "Flash", "Superman", "Green Lantern"]
+
+for i in range(100):
+    
+    first = ["Captain", "Spider", "Thor", "Iron", "Black", "Doctor", "Bat", "Wonder", "Aqua", "Flash", "Super", "Green", "JARVIS", "Star", "Scare", "Hawkeye", "Falcon"]
+    last = ["America", "Man", "Panther", "Widow", "Strange", "Vision", "Man", "Woman", "Joker", "Lantern", "Bane", "Atom", "Thanos", "Lord", "Venom", "Octopus", "Crow"]
+
+
+    firstnumber = random.randint(0, 15)
+    lastnumber = random.randint(0, 15)
+
+    print(first[firstnumber], last[lastnumber])
+
+
