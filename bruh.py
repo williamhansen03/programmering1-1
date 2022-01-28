5e68662913946d6e9fcc82ec9d93ca63907a435f
Ankan-ops <nicolas.lundberg3@gmail.com>
2022-01-28 10:55:37 +0100

    Initial code

--- bruh.py
+++ bruh.py
@@ -1,7 +1,13 @@
 @import random
 
-
+hero = 0
+villain = 0
 
 
 superhero_list = ["Captain America", "Thor", "Spiderman", "Hulk", "Iron Man", "Batman", "Superman", "Wonder Woman", "Ant Man"]
 villain_list = ["Loki", "Thanos", "Ultron", "Venom", "Joker", "Bane", "Green Goblin", "Riddler", "Darkseid", "Dr Octopus"] 
+
+
+hero = random.randint(0,8)
+villain = random.randint(0, 9)
+
