9727f5d54afce03b156972152a2b35f334228a94
fluff-otto <abbotto23@gmail.com>
2022-01-28 10:47:14 +0100

    Update bruh.py

---
+++ bruh.py
@@ -0,0 +1,7 @@
+@import random
+
+
+
+
+superhero_list = ["Captain America", "Thor", "Spiderman", "Hulk", "Iron Man", "Batman", "Superman", "Wonder Woman", "Ant Man"]
+villain_list = ["Loki", "Thanos", "Ultron", "Venom", "Joker", "Bane", "Green Goblin", "Riddler", "Darkseid", "Dr Octopus"] 
