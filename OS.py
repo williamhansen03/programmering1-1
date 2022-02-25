e47396d031a003b943a70cdecb840f7a0ef1de6f
fluff-otto <abbotto23@gmail.com>
2022-02-25 09:50:43 +0100

    Update OS.py

--- OS.py
+++ OS.py
@@ -1,4 +1,19 @@
 print("Vi vill ha en ny OS gren!")
 print("Svara ja eller nej på följande frågor:")
 
-input("Är det en bollsport?")
+sport = input("Utövas det inomhus? \n")
+
+
+if sport =="Ja":
+    sport = input("Är du introvert? \n")
+    if sport =="Ja":
+       sport = input("Är du gamer? \n")
+       if sport =="Ja":
+            print("E-sport är grenen för dig!")
+    else: 
+        sport = input("Tycker du om vatten?\n")
+        if sport =="Ja":
+            print("Hej")
+
+else:
+    print("Glöm det!")
