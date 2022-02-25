7b708dbd0dba19e027fe165bd61c884ff87ae4a1
williamhansen03 <91659748+williamhansen03@users.noreply.github.com>
2022-02-25 11:35:34 +0100

    Update OS.py

--- OS.py
+++ OS.py
@@ -2,43 +2,77 @@ print("Vi vill ha en ny OS gren!")
 print("Svara Ja eller Nej på följande frågor:")
 
 
-sport = input("Är det en sport?")
+sport = input("Är det en sport?\n")
 
-if sport == "ja":
-    sport = input("Är det en lagsport?")
-    if sport == "nej":
-        sport = input("Är det en bollsport?")
-        if sport == "ja":
-            print("Synd, alla är upptagna!")
-else:
-    print("Glöm det!")
+if sport == "Ja":
+    sport = input("Är det en lagsport?\n")
+    if sport == "Nej":
+        sport = input("Är det en bollsport?\n")
+        if sport == "Ja":
+            print("Synd, alla är upptagna!\n")
+        elif sport == "Nej": 
+            sport = input("Tycker du om vatten?\n")
 
-sport = input("Utövas det inomhus? \n")
-
-
-if sport =="Ja":
-    sport = input("Är du introvert? \n")
-
-    if sport =="Ja":
-       sport = input("Är du gamer? \n")
-
-       if sport =="Ja":
-            print("E-sport är grenen för dig!")
-    else: 
-        sport = input("Tycker du om vatten?\n")
-
-        if sport =="Ja":
-            sport = input("Tycker du om fisk? \n")
-
-            if sport == "Ja":
-                print("OS-FISKE!")
-
+            if sport == "Nej":
+                sport = input("Är du gamer? \n")
+                if sport =="Ja":
+                        print("E-sport är grenen för dig!")
+            elif sport == "Ja":
+                sport = input("Tycker du om fisk? \n")
                 if sport == "Ja":
-                    print("Grubbla över OS-grenar")
+                    print("OS-FISKE!") 
                 elif sport == "Nej":
-                    print("OS-DRUNKNING")
+                    sport = input("Tycker du om att simma? \n")
+                    if sport == "Ja":
+                        print("Grubbla över OS-grenar")
+                    elif sport == "Nej":
+                        print("OS-DRUNKNING")
+    elif sport == "Ja":
+        sport = input("Utövas det inomhus? \n")
+        if sport =="Ja":
+            sport = input("Är du introvert? \n")
+            if sport =="Ja":
+                sport = input("Är du gamer? \n")
+                if sport =="Ja":
+                    print("E-sport är grenen för dig!")
+            elif sport == "Nej": 
+                sport = input("Tycker du om vatten?\n")
 
-            elif sport == "Nej":
-                sport = input("Tycker du om att simma? \n")
+                if sport == "Nej":
+                    sport = input("Är du gamer? \n")
+
+                    if sport =="Ja":
+                        print("E-sport är grenen för dig!")
+                elif sport == "Ja":
+                    sport = input("Tycker du om fisk? \n")
+                    if sport == "Ja":
+                        print("OS-FISKE!")
+                    elif sport == "Nej":
+                        sport = input("Tycker du om att simma? \n")
+                        if sport == "Ja":
+                            print("Grubbla över OS-grenar")
+                        elif sport == "Nej":
+                            print("OS-DRUNKNING")
+        elif sport == "Nej":
+            sport = input("Är det en bollsport?\n")
+            if sport == "Ja":
+                print("Synd, alla är upptagna!\n")
+            elif sport == "Nej": 
+                sport = input("Tycker du om vatten?\n")
+
+                if sport == "Nej":
+                    sport = input("Är du gamer? \n")
+                    if sport =="Ja":
+                            print("E-sport är grenen för dig!")
+                elif sport == "Ja":
+                    sport = input("Tycker du om fisk? \n")
+                    if sport == "Ja":
+                        print("OS-FISKE!") 
+                    elif sport == "Nej":
+                        sport = input("Tycker du om att simma? \n")
+                        if sport == "Ja":
+                            print("Grubbla över OS-grenar")
+                        elif sport == "Nej":
+                            print("OS-DRUNKNING")
 else:
     print("Glöm det!")
